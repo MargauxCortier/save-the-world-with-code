fba44e5eeac923f281296b3dff722c0df855004b
Margaux <cortier.margaux@gmail.com>
2018-11-14 17:54:15 +0100

    add landing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'landing_page/lpfirst'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/landing_page_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class LandingPageControllerTest < ActionDispatch::IntegrationTest
+  test "should get lpfirst" do
+    get landing_page_lpfirst_url
+    assert_response :success
+  end
+
+end

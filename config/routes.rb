06109a2cc0e37b52ce59d9a40450fe7c3b7c608f
Margaux <cortier.margaux@gmail.com>
2018-11-15 18:19:59 +0100

    iui

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
 	
   get '/', to: 'landing_page#lpfirst'
+  post '/', to: 'landing_page#lpfirst'
 
   resources :new_users
-  get 'landing_page/lpfirst'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

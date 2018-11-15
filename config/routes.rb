4ed3cb0a957f0faad2e6e1f1aa19fd1ec27cb5d7
Margaux <cortier.margaux@gmail.com>
2018-11-15 23:34:50 +0100

    le chnord

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
 
 	root 'landing_page#lpfirst'
   get '/', to: 'landing_page#lpfirst'
+  post '/', to:'landing_page#create'
 
   get '/diplomé', to: 'landing_page#lpseconde'
+  post '/diplomé', to: 'landing_page#lpseconde'
 
-  post '/', to:'landing_page#create'
+  get '/nord', to: 'landing_page#lpthird'
+  post '/nord', to: 'landing_page#lpthird'
+
   resources :new_users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

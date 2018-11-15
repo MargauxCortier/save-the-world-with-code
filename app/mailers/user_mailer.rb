2a60a3c08d5e9d66b457e5219a7ba596f8588cd1
Hélène <helene.cbm@gmail.com>
2018-11-15 14:31:30 +0100

    3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,18 +1,11 @@
 class UserMailer < ApplicationMailer
-<<<<<<< HEAD
-  def welcome_email(user)
-     @user = user
-     mail(to: @user.email, subject: 'Welcome to THP babe')
-   end
-  
-=======
+
 
 	def welcome_email
     @user = params[:user]
     #@url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to THP babe fdp')
   end
 
 
->>>>>>> 9ad2b7e428c7773add72ebc53cc737a711332821
 end
